e9cb0117d1ad1395c567c20b749c0b6c80530eb6
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-09-09 15:49:01 +0000

    call super().save() unconditionally in WorkshopCategory to ensure categories save correctly in admin

--- workshops/models.py
+++ workshops/models.py
@@ -6,8 +6,10 @@ from django.utils.text import slugify
 class WorkshopCategory(models.Model):
     name=models.CharField(max_length=80, unique=True)
     slug=models.SlugField(max_length=90, unique=True, blank=True)
-    def save(self,*a,**k):
-        if not self.slug: self.slug=slugify(self.name); super().save(*a,**k)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     def __str__(self): return self.name
 
 class Instructor(models.Model):
