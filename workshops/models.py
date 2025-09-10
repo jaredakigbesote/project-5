5c099a58704b915bdbf18449ee5a7c9f1e2eb4a8
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-09-10 16:09:03 +0000

    import timezone from django.utils.timezone (not django.utils.text)

--- workshops/models.py
+++ workshops/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.db.models import F
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.text import slugify, timezone
+from django.utils.text import slugify
+from django.utils import timezone
 
 class WorkshopCategory(models.Model):
     name=models.CharField(max_length=80, unique=True)
